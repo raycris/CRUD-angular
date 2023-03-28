9f9cfecb3a70f0ed91ad284c28e488f8ce664b62
Raycris Maldonado <raycris.maldonado@utreee.com>
2023-03-28 16:39:54 -0400

    dark mode done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
 
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'CRUD-angular';
+
+  chageTheme() {
+    const element = document.body;
+    element.dataset.bsTheme =
+      element.dataset.bsTheme == 'light' ? 'dark' : 'light';
+  }
 }
-
-
